bd06e3ce77deef5a3c10c0d31f9558562db12cd5
Walidoux <ma.walidkorchi@icloud.com>
2022-02-12 23:07:11 +0100

    🚀 init: initial boilerplate

---
+++ components/Head.tsx
@@ -0,0 +1,42 @@
+import NextHead from "next/head";
+
+const Head: React.FC = () => {
+  const data = {
+    title: "Project",
+    image: "/images/icons/64x64.png",
+    description: "Your website description here.",
+    url: "https://urlwebsite.fr/",
+  };
+
+  return (
+    <NextHead>
+      <title>Walid Korchi</title>
+
+      <link rel="icon" type="image/png" href={data.image} />
+      <link rel="apple-touch-icon" href={data.image} />
+
+      {/* Meta Tag */}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content={data.description} />
+      <meta name="Language" content="fr, en" />
+      <meta name="theme-color" content="#27B05E" />
+
+      {/* Open Graph Metadata */}
+      <meta property="og:title" content={data.title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={data.url} />
+      <meta property="og:image" content={data.image} />
+      <meta property="og:description" content={data.description} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:site_name" content={data.title} />
+
+      {/* Twitter card Metadata */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:description" content={data.description} />
+      <meta name="twitter:title" content={data.title} />
+      <meta name="twitter:image:src" content={data.image} />
+    </NextHead>
+  );
+};
+
+export default Head;
