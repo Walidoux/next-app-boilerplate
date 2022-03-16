6a754b4e4a2b7c7d3d67fd6355bbce4dbf21e6aa
Walidoux <ma.walidkorchi@icloud.com>
2022-03-16 18:55:39 +0100

    fix(styles): remove useless import fonts file +edit config stylelint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 
 import "../styles/main.scss";
-import "../styles/fonts.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
