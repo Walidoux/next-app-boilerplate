b49f86ae28770b5a18980769f8f91c92eafe87ad
Walidoux <ma.walidkorchi@icloud.com>
2022-03-17 12:26:12 +0100

    upgrade(nextjs): nextjs version upgraded

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
   sassOptions: {
     includePaths: [
       path.join(__dirname, 'styles/abstracts'),
