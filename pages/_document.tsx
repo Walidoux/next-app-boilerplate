a3efc793c0d61e6f1b9c8a89562cce95d3c07548
Walidoux <ma.walidkorchi@icloud.com>
2022-03-24 11:38:07 +0100

    refactor(pages): improve _document.tsx types with default locale

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,17 @@
-import { Html, Main, Head, NextScript } from 'next/document'
+import Document, { Html, Main, Head, NextScript } from 'next/document'
 
-const NextDocument: React.FC = (): JSX.Element => {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default NextDocument
+export default MyDocument
