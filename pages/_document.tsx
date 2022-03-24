3231c61c123c60314201f3ebacafe06fca21bede
Walidoux <ma.walidkorchi@icloud.com>
2022-03-24 01:52:23 +0100

    feat(pages): add _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Main, Head, NextScript } from 'next/document'
+
+const NextDocument: React.FC = (): JSX.Element => {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default NextDocument
