226b1542bb738cf4c22f1d6a8c91eb22d0d99d09
Walidoux <ma.walidkorchi@icloud.com>
2022-04-05 03:08:50 +0000

    refactor(pages): add content for the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
 
-import Head from '../components/Head/NextHead'
+import Head from 'components/Head/NextHead'
+
+import styles from '../styles/pages/home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <main className={styles.application}>
       <Head />
-    </>
+
+      <p className={styles.message}>Hello Beautiful World of Coding !</p>
+      <span className={styles.message}>Some sort of tests</span>
+    </main>
   )
 }
 
