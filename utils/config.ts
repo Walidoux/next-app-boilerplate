816877fd9573c4deefd044cb235770ae56705797
Walidoux <ma.walidkorchi@icloud.com>
2022-04-12 14:58:36 +0000

    refactor(config): add png Logo support and change color project

--- utils/config.ts
+++ utils/config.ts
@@ -6,9 +6,9 @@ export const projectConfig: PCDefaultValues = {
   description:
     'The latest Next.js boilerplate starter code/time saver with Typescript / SCSS. Including linters such as: Prettier, Eslint and Stylelint.',
   url: 'Unknown project url',
-  color: '#441F83',
+  color: '#775aaa',
   icons: {
-    image: '/images/Logo.svg',
+    image: '/images/Logo.png',
     default: '/icons/favicons/favicon.ico',
     apple: '/icons/favicons/apple-touch-icon.png',
     '16_16': '/icons/favicons/favicon-16x16.png',
