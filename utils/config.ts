637b60da319983f93db68f956a61e95eeb649ad0
Walidoux <ma.walidkorchi@icloud.com>
2022-04-12 14:44:40 +0000

    refactor: config nextjs boilerplate

--- utils/config.ts
+++ utils/config.ts
@@ -1,13 +1,14 @@
 import { PCDefaultValues } from 'types/projectConfig'
 
 export const projectConfig: PCDefaultValues = {
-  shortName: 'Unknown project name',
-  longName: 'Unknown project name',
-  description: 'Unknown project description',
+  shortName: 'Boilerplate',
+  longName: 'Next.Js Boilerplate',
+  description:
+    'The latest Next.js boilerplate starter code/time saver with Typescript / SCSS. Including linters such as: Prettier, Eslint and Stylelint.',
   url: 'Unknown project url',
-  color: '#FF0000',
+  color: '#441F83',
   icons: {
-    image: '/images/Logo.png',
+    image: '/images/Logo.svg',
     default: '/icons/favicons/favicon.ico',
     apple: '/icons/favicons/apple-touch-icon.png',
     '16_16': '/icons/favicons/favicon-16x16.png',
